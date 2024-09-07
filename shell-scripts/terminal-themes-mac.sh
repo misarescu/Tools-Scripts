5ee8d0cf81a470e22504e1944f5473e4b1a32c08
redshadowfx <mihai.isarescu@protonmail.com>
2024-09-07 11:18:46 +0300

    update terminal themes switcher and add mac version

--- shell-scripts/terminal-themes-mac.sh
+++ shell-scripts/terminal-themes-mac.sh
@@ -11,6 +11,6 @@ else
     sed_replace_str_capital='s/Mocha/Latte/g'
 fi
 
-sed -i -e "${sed_replace_str}" ~/.tmux.conf
-sed -i -e "${sed_replace_str}" ~/.config/k9s/config.yaml
-sed -i -e "${sed_replace_str_capital}" ~/.config/bat/config
+sed -i '' -e "${sed_replace_str}" ~/.tmux.conf
+sed -i '' -e "${sed_replace_str}" ~/Library/Application\ Support/k9s/config.yaml
+sed -i '' -e "${sed_replace_str_capital}" ~/.config/bat/config
