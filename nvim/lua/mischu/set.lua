9527f3d1cedb4628c43f647fd739905a514cc482
misarescu <misarescu@deviqon.com>
2025-03-03 14:42:49 +0200

    fix(nvim): typo that duplicates filename

--- nvim/lua/mischu/set.lua
+++ nvim/lua/mischu/set.lua
@@ -68,7 +68,7 @@ require('lualine').setup({
   },
   sections = {
     lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-    lualine_b = { { 'filename', path = 1 }, 'filename', 'branch' },
+    lualine_b = { { 'filename', path = 1 }, 'branch' },
     lualine_c = {
       '%=', --[[ add your center compoentnts here in place of this comment ]]
     },
