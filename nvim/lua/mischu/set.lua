80c1d0973bb1caf6d3e3fc0ac693ce7d0e2ab655
misarescu <misarescu@deviqon.com>
2025-03-13 17:48:40 +0200

    feat(nvim): add line highlighting

--- nvim/lua/mischu/set.lua
+++ nvim/lua/mischu/set.lua
@@ -8,6 +8,7 @@ vim.cmd.colorscheme("catppuccin")
 vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
 vim.opt.nu = true                 -- set line numbers -- set line numbers
 vim.opt.relativenumber = true     -- use relative line numbers
+vim.opt.cursorline = true
 -- set tab size to 2 spaces
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
